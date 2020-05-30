b3379e8536e061173b347503eb7cc4d52dafa95e
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-30 04:46:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,9 @@ class Student
   end
   
   def self.create_table
-      
+    sql = <<-SQL
+    
+    SQL
   end# Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
