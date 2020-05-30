09359e17a31223df1857a0e80f0e867f41261ccd
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-30 04:49:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE (
+      CREATE TABLE IF NOT EXISTS songs (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
