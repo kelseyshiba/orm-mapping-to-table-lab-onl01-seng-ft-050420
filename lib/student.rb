d44f7f36a46bcba1dcee7f14f1f5a36b09fce38a
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-05-30 04:56:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,7 +10,7 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS songs (
+      CREATE TABLE IF NOT EXISTS students (
       id INTEGER PRIMARY KEY,
       name TEXT,
       grade INTEGER
@@ -22,7 +22,7 @@ class Student
   
   def self.drop_table
     sql = <<-SQL
-      DROP TABLE songs;
+      DROP TABLE students;
     SQL
     
     DB[:conn].execute(sql)
@@ -30,7 +30,7 @@ class Student
   
   def save
     sql = <<-SQL
-      INSERT INTO songs (name, grade) VALUES (?, ?)
+      INSERT INTO students (name, grade) VALUES (?, ?)
     SQL
     
     DB[:conn].execute(sql, self.name, self.grade)
